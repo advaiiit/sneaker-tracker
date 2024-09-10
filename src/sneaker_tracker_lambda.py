470b74b185d548ae6f81f935005033cad2d28c97
KallolBairagi <kbairagi@relatient.com>
2024-09-10 19:26:57 +0530

    separate out flipkart and lazada api service

--- src/sneaker_tracker_lambda.py
+++ src/sneaker_tracker_lambda.py
@@ -1,46 +1,23 @@
 import requests
+import os
+from flask import Flask, request, jsonify
+from api_service.flipkart_api_service import product_search as flipkart_ps
+from api_service.lazada_api_service import product_search as lazada_ps
 
-def lambda_handler(event, context):
+app = Flask(__name__)
 
-    # testing flipkart api
-    url = 'https://real-time-flipkart-api.p.rapidapi.com/product-search'
-    querystring = {
-        "q": "new balance 2002",
-        "page": "1",
-        "sort_by": "popularity"
+@app.route('/search', methods=['GET'])
+def search():
+    param = request.args.get('word')
+    res1 = flipkart_ps(param)
+    res2 = lazada_ps(param)
+    combined_results = {
+        "flipkart_results": res1,
+        "lazada_results": res2
     }
 
-    headers = {
-        'x-rapidapi-host': 'real-time-flipkart-api.p.rapidapi.com',
-        'x-rapidapi-key':  os.environ.get("API_KEY")
-    }
+    return res1
 
-    try:
-        response = requests.get(url, headers=headers, params=querystring)
-        
-        if response.status_code == 200:
-            data = response.json()
-            return {
-                'statusCode': 200,
-                'body': data
-            }
-        else:
-            return {
-                'statusCode': response.status_code,
-                'body': f"Failed to fetch data. Status code: {response.status_code}"
-            } 
-    except requests.exceptions.RequestException as e:
-        return {
-            'statusCode': 500,
-            'body': f"An error occurred: {e}"
-        }
-
-def main():
-    with open('test_event.json') as f:
-        event = f.read()
-    result = lambda_handler(eval(event), None)
-    print(result)
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
   
